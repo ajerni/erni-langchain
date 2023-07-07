ae8dde086f1949e6fd1606b04593d4889b0af2e2
„Andi <ajerni@gmail.com>
2023-07-07 16:39:42 +0200

    triggger

--- pages/2_Agents.py
+++ pages/2_Agents.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.header("Agents - ...coming soon...")
 
-st.text("Build an agent that uses differen tools, also own ones.")
+st.text("Build an agent that uses different tools, also own ones.")
