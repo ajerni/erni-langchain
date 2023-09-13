67c652c56720b3d929f58581784cfa0dd081aff2
„Andi <ajerni@gmail.com>
2023-09-13 08:15:14 +0200

    menu tests

--- Home.py
+++ Home.py
@@ -3,6 +3,13 @@ import streamlit as st
 st.set_page_config(
     page_title="Andi's Langchain Projects",
     page_icon="👋",
+    # layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'Get Help': 'https://www.extremelycoolapp.com/help',
+        'Report a bug': "https://www.extremelycoolapp.com/bug",
+        'About': "# This is a header. This is an *extremely* cool app!"
+    }
 )
 
 st.write("# Andi's Langchain Projects 🤖")
